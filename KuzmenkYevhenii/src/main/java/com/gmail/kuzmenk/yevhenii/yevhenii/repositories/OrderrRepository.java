1c75aa7c5978ea18c56cf5e3a755432536449b18
YevheniiKuzmenk <95362110+YevheniiKuzmenk@users.noreply.github.com>
2021-12-14 16:25:30 +0200

    Delete KuzmenkYevhenii directory

--- KuzmenkYevhenii/src/main/java/com/gmail/kuzmenk/yevhenii/yevhenii/repositories/OrderrRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gmail.kuzmenk.yevhenii.yevhenii.repositories;
-
-import com.gmail.kuzmenk.yevhenii.yevhenii.models.Orderr;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderrRepository extends JpaRepository<Orderr, Long> {
-
-
-}
